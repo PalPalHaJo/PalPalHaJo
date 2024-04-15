acf42b68cf5acf9df3e4435d72cfda085c7a8ace
BY0808 <166494058+BY0808@users.noreply.github.com>
2024-04-15 20:21:00 +0900

    Revert "Lee kh"

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -4,20 +4,18 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    int idx = 0;
-    //카드의 애니메이터 변수
-    Animator animator;
-    //앞면과 뒷면 오브젝트
-    GameObject[] cards = new GameObject[2];
+    public int idx = 0;
+
+    public GameObject front;
+    public GameObject back;
+
+    public Animator anim;
+
+    public SpriteRenderer FrontImage;
     // Start is called before the first frame update
     void Start()
     {
-        //애니메이터 컴포넌트 가져오기
-        animator = GetComponent<Animator>();
-        //이 오브젝트의 첫번째 자식인 오브젝트를 cards배열 0번째에 대입
-        cards[0] = transform.GetChild(0).gameObject;
-        //이 오브젝트의 두번째 자식인 오브젝트를 cards배열 1번째에 대입
-        cards[1] = transform.GetChild(1).gameObject;
+        
     }
 
     // Update is called once per frame
@@ -25,20 +23,49 @@ public class Card : MonoBehaviour
     {
         
     }
-    
-    public void OpenCard()
-    {
-        //파라미터 IsOpen을 true 상태로 변경하여 애니메이션이 재생되게 한다.
-        animator.SetBool("IsOpen", true);
-        //두번째 자식(Back)을 비활성화 한다.
-        cards[1].SetActive(false);
-        //첫번째 자식(Front)를 활성화 한다.
-        cards[0].SetActive(true);
-    }
-    
+
     public void Setting(int number)
     {
-        idx = number;
-        Resources.Load<Sprite>($"TeamEight{idx}");
+      idx = number;
+        FrontImage.sprite = Resources.Load<Sprite>($"TeamEight{idx}");
+    }
+
+    public void OpenCard()
+    {
+        anim.SetBool("isOpen", true);
+        front.SetActive(true);
+        back.SetActive(false);
+
+        if(GameManager.instance.firstCard == null)
+        {
+            GameManager.instance.firstCard = this;
+        }
+
+        else
+        {
+            GameManager.instance.secondCard = this;
+            GameManager.instance.Matched();
+        }
+    }
+
+    public void DestoryCard()
+    {
+        Invoke("DestroyCardInvok", 1.0f);
+    }
+
+    void DestroyCardInvoke()
+    {
+        Destroy(gameObject);
+    }
+    public void CloseCard()
+    {
+        Invoke("CloseCardInvok", 1.0f);
+    }
+
+    void CloseCardInvok()
+    {
+        anim.SetBool("isOpen", false);
+        front.SetActive(false);
+        back.SetActive(true);
     }
 }
