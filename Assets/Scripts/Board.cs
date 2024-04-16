61be3b175259e032af5da5fd4a756b9614a479cf
w4733 <w4733@naver.com>
2024-04-16 17:12:42 +0900

    보드 스크립트 수정
    코드스니펫을 활용하지 않고 직접 만들어 보기

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -19,22 +19,38 @@ public class Board : MonoBehaviour
 
     void Start()
     {
+        // 카드 숫자 배열 생성
         int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
-        arr = arr.OrderBy(x => Random.Range(0f, 7f)).ToArray();
+        // 카드 숫자 배열을 무작위로 섞는다
+        ShuffleArray(arr);
 
         // 카드 배치, 한 줄당 4장씩
-        for (int i = 0; i< totalCardCnt; i++)
+        for (int i = 0; i < totalCardCnt; i++)
         {
             // 좌표 계산
-            float x = (i % 4) * interval + startX; 
+            float x = (i % 4) * interval + startX;
             float y = (i / 4) * interval + startY;
 
             GameObject go = Instantiate(card, this.transform); // 임시 변수
-            go.transform.position = new Vector2(x,y); // 배치
+            go.transform.position = new Vector2(x, y); // 배치
             go.GetComponent<Card>().Setting(arr[i]);
         }
 
         //게임끝내기 위한 배열 수 읽어오기
         GameManager.instance.cardCount = arr.Length;
+    }
+    // Fisher-Yates 셔플 알고리즘을 사용하여 무작위로 섞는 함수
+    void ShuffleArray<T>(T[] array)
+    {
+        int n = array.Length;
+        for (int i = 0; i < n; i++)
+        {
+            // 현재 인덱스부터 배열 끝까지 중에서 무작위로 다음 인덱스를 선택
+            int r = i + Random.Range(0, n - i);
+            // 선택된 인덱스의 값과 현재 인덱스의 값을 교환
+            T temp = array[r];
+            array[r] = array[i];
+            array[i] = temp;
+        }
     }
 }
