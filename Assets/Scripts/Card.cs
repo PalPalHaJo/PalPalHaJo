f5a337acc15c19ef8b9ef770283c29864934f2e6
Euronia12 <lkh0647@naver.com>
2024-04-16 11:34:11 +0900

    이미지 사이즈 변경 및 카드 되돌리기
    3번이미지를 100 by 100으로 변경 후 애니메이션 재등록하였습니다.
    첫번째 카드 선택 후 3초가 지나도록 2번째 카드를 선택하지 않으면 카드를 되돌리도록 구현하였습니다.

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -14,11 +14,14 @@ public class Card : MonoBehaviour
     [SerializeField]
     float fDelayTime = 1.0f;
 
+    [SerializeField]
+    float fCountDownTime = 3.0f;
+
     public SpriteRenderer FrontImage;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -42,12 +45,27 @@ public class Card : MonoBehaviour
         if(GameManager.instance.firstCard == null)
         {
             GameManager.instance.firstCard = this;
+            //두번쨰 카드 선택까지 카운트 다운하는 코루틴 시작
+            StartCoroutine(CountDown());
         }
         else
         {
+            //두번째 카드 선택시 코루틴 중단
+            StopCoroutine(CountDown());
             GameManager.instance.secondCard = this;
             GameManager.instance.Matched();
         }
+    }
+
+    //5초 후 첫번째 선택하는 카드를 되돌림
+    IEnumerator CountDown()
+    {
+        //'fCountDownTime'초 대기 후
+        yield return new WaitForSeconds(fCountDownTime);
+        //게임매니저의 첫번째 카드에 등록된 정보를 초기화
+        GameManager.instance.firstCard = null;
+        //해당카드를 뒤집기 위한 메소드
+        CloseCard();
     }
 
     //두 카드의 인덱스가 같을 시 호출되어 카드를 파괴하는 함수
