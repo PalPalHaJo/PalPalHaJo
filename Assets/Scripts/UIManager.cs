a44b368a85381ccf4b2f5e6e41cbf17fd896cdf2
Euronia12 <lkh0647@naver.com>
2024-04-18 17:19:36 +0900

    주석 추가 입력

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,23 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class UIManager
 {
+    //게임 실행을 종료
     public void OnApplicationQuit()
     {
         Application.Quit();
     }
 
+    //오브젝트 활성화
     public void OnUIPanerl(GameObject go)
     {
         Time.timeScale = 0.0f;
         go.SetActive(true);
     }
 
+    //오브젝트 비활성화
     public void OffUIPanerl(GameObject go)
     {
         Time.timeScale = 1.0f;
         go.SetActive(false);
     }
 
+    // 씬전환
     public void TransitionScene(int SceneNum)
     {
         Time.timeScale = 1.0f;
