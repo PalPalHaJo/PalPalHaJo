03d624ec81c5fcfe1d274d66d3ddf7160ad52858
Euronia12 <lkh0647@naver.com>
2024-04-18 16:00:41 +0900

    스테이지매니저 제외 완성본

--- Assets/Scripts/BtnEvents.cs
+++ Assets/Scripts/BtnEvents.cs
@@ -7,7 +7,7 @@ public class BtnEvents : MonoBehaviour
 {
     public Button startBtn, quitBtn, returnBtn;
     public Button stage1, stage2, stage3, stage4;
-
+    public StageManager stageManager;
     public GameObject stagePanel;
     // Start is called before the first frame update
     void Start()
@@ -20,10 +20,10 @@ public class BtnEvents : MonoBehaviour
         startBtn.onClick.AddListener(() => SystemManager.ui.OnUIPanerl(stagePanel));
         quitBtn.onClick.AddListener(() => SystemManager.ui.OnApplicationQuit());
         returnBtn.onClick.AddListener(() => SystemManager.ui.OffUIPanerl(stagePanel));
-        stage1.onClick.AddListener(() => SystemManager.ui.TransitionScene(1));
-        stage2.onClick.AddListener(() => SystemManager.ui.TransitionScene(2));
-        stage3.onClick.AddListener(() => SystemManager.ui.TransitionScene(3));
-        stage4.onClick.AddListener(() => SystemManager.ui.TransitionScene(4));
+        stage1.onClick.AddListener(() => stageManager.StartStage());
+        stage2.onClick.AddListener(() => stageManager.StartStage());
+        stage3.onClick.AddListener(() => stageManager.StartStage());
+        stage4.onClick.AddListener(() => stageManager.StartStage());
     }
 
 }
